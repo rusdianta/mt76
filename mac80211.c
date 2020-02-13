5999225ee4d50c49ca7e72fe40f7d3fd9ef519f0
Felix Fietkau <nbd@nbd.name>
2020-02-13 22:56:33 +0100

    mt76: do not set HOST_BROADCAST_PS_BUFFERING for mt7615 mt7615 handles powersave buffering in firmware

--- mac80211.c
+++ mac80211.c
@@ -333,7 +333,6 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 
 	ieee80211_hw_set(hw, SIGNAL_DBM);
 	ieee80211_hw_set(hw, PS_NULLFUNC_STACK);
-	ieee80211_hw_set(hw, HOST_BROADCAST_PS_BUFFERING);
 	ieee80211_hw_set(hw, AMPDU_AGGREGATION);
 	ieee80211_hw_set(hw, SUPPORTS_RC_TABLE);
 	ieee80211_hw_set(hw, SUPPORT_FAST_XMIT);

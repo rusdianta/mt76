5991176eb075f9c0934db779a8861b297db4cf0e
Ryder Lee <ryder.lee@mediatek.com>
2021-03-30 01:25:00 +0800

    mt76: report Rx timestamp Frame reception timestamp (low 32-bits) that indicates the value of the local TSF timer value at the time the first bit of the MAC header in the received frame (PPDU unit) arriving at the MAC.
    Signed-off-by: Ryder Lee <ryder.lee@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 44aa3cc06ac183154f1be4eb1f4d40dd63a1ca33)
    (cherry picked from commit ce06a4505d5632dae1ca062ba719cb94a0b65210)
    (cherry picked from commit 99b73d812dbef5e26c099e9e6e92aafe3e407a2a)
    (cherry picked from commit 0d5964ad12a9615626fb7ae183b16dd5701a461d)
    (cherry picked from commit 1fa8e8cc3078b76dfbcff3940259309bf6503b11)

--- mac80211.c
+++ mac80211.c
@@ -613,6 +613,8 @@ static struct ieee80211_sta *mt76_rx_convert(struct sk_buff *skb)
 	status->signal = mstat.signal;
 	status->chains = mstat.chains;
 	status->ampdu_reference = mstat.ampdu_ref;
+	status->device_timestamp = mstat.timestamp;
+	status->mactime = mstat.timestamp;
 
 	if (ieee80211_is_beacon(hdr->frame_control) ||
 	    ieee80211_is_probe_resp(hdr->frame_control))

908ba4748f77d1b5ac17cd7203fb3ba21139760e
Felix Fietkau <nbd@nbd.name>
2021-10-11 12:07:46 +0200

    mt76: do not access 802.11 header in ccmp check for 802.3 rx skbs Avoids false positive on detecting frags or encrypted mgmt frames
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mac80211.c
+++ mac80211.c
@@ -650,6 +650,10 @@ mt76_check_ccmp_pn(struct sk_buff *skb)
 	if (!wcid || !wcid->rx_check_pn)
 		return 0;
 
+	security_idx = status->qos_ctl & IEEE80211_QOS_CTL_TID_MASK;
+	if (status->flag & RX_FLAG_8023)
+		goto skip_hdr_check;
+
 	hdr = (struct ieee80211_hdr *)skb->data;
 	if (!(status->flag & RX_FLAG_IV_STRIPPED)) {
 		/*
@@ -670,9 +674,8 @@ mt76_check_ccmp_pn(struct sk_buff *skb)
 	if (ieee80211_is_mgmt(hdr->frame_control) &&
 	    !ieee80211_has_tods(hdr->frame_control))
 		security_idx = IEEE80211_NUM_TIDS;
-	else
-		security_idx = status->qos_ctl & IEEE80211_QOS_CTL_TID_MASK;
 
+skip_hdr_check:
 	BUILD_BUG_ON(sizeof(status->iv) != sizeof(wcid->rx_key_pn[0]));
 	ret = memcmp(status->iv, wcid->rx_key_pn[security_idx],
 		     sizeof(status->iv));

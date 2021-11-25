9eefe26ac3ab2d549af45edeaafb7a73b1693803
Daniel Golle <daniel@makrotopia.org>
2021-11-25 19:45:03 +0000

    mt76: eeprom: tolerate corrected bit-flips
    mtd_read() returns -EUCLEAN in case of corrected bit-flips.
    As data was read, don't error out in this case.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Acked-by: Hauke Mehrtens <hauke@hauke-m.de>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 25237b19bcc134490a8e1c6b809ef7a91f1219ce)

--- eeprom.c
+++ eeprom.c
@@ -56,7 +56,7 @@ mt76_get_of_eeprom(struct mt76_dev *dev, int len)
 	offset += be32_to_cpup(list);
 	ret = mtd_read(mtd, offset, len, &retlen, dev->eeprom.data);
 	put_mtd_device(mtd);
-	if (ret) {
+	if (ret && !mtd_is_bitflip(ret)) {
 		dev_err(dev->dev, "reading EEPROM from mtd %s failed: %i\n",
 			part, ret);
 		goto out_put_node;

d087d9935b588290444ad11e53cb7ccb84ad1cf2
Felix Fietkau <nbd@nbd.name>
2022-03-19 21:40:14 +0100

    mt76: reduce tx queue lock hold time - call txq dequeue without holding txq lock (locking handled by mac80211) - disable bh around tx queue schedule
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- tx.c
+++ tx.c
@@ -411,7 +411,9 @@ mt76_txq_send_burst(struct mt76_dev *dev, struct mt76_queue *q,
 		ieee80211_get_tx_rates(txq->vif, txq->sta, skb,
 				       info->control.rates, 1);
 
+	spin_lock(&q->lock);
 	idx = __mt76_tx_queue_skb(dev, qid, skb, wcid, txq->sta, &stop);
+	spin_unlock(&q->lock);
 	if (idx < 0)
 		return idx;
 
@@ -431,14 +433,18 @@ mt76_txq_send_burst(struct mt76_dev *dev, struct mt76_queue *q,
 			ieee80211_get_tx_rates(txq->vif, txq->sta, skb,
 					       info->control.rates, 1);
 
+		spin_lock(&q->lock);
 		idx = __mt76_tx_queue_skb(dev, qid, skb, wcid, txq->sta, &stop);
+		spin_unlock(&q->lock);
 		if (idx < 0)
 			break;
 
 		n_frames++;
 	} while (1);
 
+	spin_lock(&q->lock);
 	dev->queue_ops->kick(dev, q);
+	spin_unlock(&q->lock);
 
 	return n_frames;
 }
@@ -472,8 +478,6 @@ mt76_txq_schedule_list(struct mt76_dev *dev, enum mt76_txq_id qid)
 		if (wcid && test_bit(MT_WCID_FLAG_PS, &wcid->flags))
 			continue;
 
-		spin_lock_bh(&q->lock);
-
 		if (mtxq->send_bar && mtxq->aggr) {
 			struct ieee80211_txq *txq = mtxq_to_txq(mtxq);
 			struct ieee80211_sta *sta = txq->sta;
@@ -482,16 +486,12 @@ mt76_txq_schedule_list(struct mt76_dev *dev, enum mt76_txq_id qid)
 			u8 tid = txq->tid;
 
 			mtxq->send_bar = false;
-			spin_unlock_bh(&q->lock);
 			ieee80211_send_bar(vif, sta->addr, tid, agg_ssn);
-			spin_lock_bh(&q->lock);
 		}
 
 		if (!mt76_txq_stopped(q))
 			n_frames = mt76_txq_send_burst(dev, q, mtxq);
 
-		spin_unlock_bh(&q->lock);
-		
 		ieee80211_return_txq(dev->hw, txq, false);
 
 		if (unlikely(n_frames < 0))
@@ -510,6 +510,7 @@ void mt76_txq_schedule(struct mt76_dev *dev, enum mt76_txq_id qid)
 	if (qid >= 4)
 		return;
 
+	local_bh_disable();
 	rcu_read_lock();
 
 	do {
@@ -519,6 +520,7 @@ void mt76_txq_schedule(struct mt76_dev *dev, enum mt76_txq_id qid)
 	} while (len > 0);
 
 	rcu_read_unlock();
+	local_bh_enable();
 }
 EXPORT_SYMBOL_GPL(mt76_txq_schedule);
 

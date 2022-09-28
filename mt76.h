cfacaa1bdf1dd2f4a96654dce7683c57bd075f9c
Felix Fietkau <nbd@nbd.name>
2022-09-28 10:49:11 +0200

    wifi: mt76: move mt76_rate_power from core to mt76x02 driver code Its layout and code is mt76x02 specific

--- mt76.h
+++ mt76.h
@@ -404,19 +404,6 @@ struct mt76_sband {
 	struct mt76_channel_state *chan;
 };
 
-struct mt76_rate_power {
-	union {
-		struct {
-			s8 cck[4];
-			s8 ofdm[8];
-			s8 stbc[10];
-			s8 ht[16];
-			s8 vht[10];
-		};
-		s8 all[48];
-	};
-};
-
 /* addr req mask */
 #define MT_VEND_TYPE_EEPROM	BIT(31)
 #define MT_VEND_TYPE_CFG	BIT(30)
@@ -595,7 +582,6 @@ struct mt76_dev {
 	struct debugfs_blob_wrapper otp;
 	struct mt76_hw_cap cap;
 
-	struct mt76_rate_power rate_power;
 	int txpower_cur;
 
 	char alpha2[3];

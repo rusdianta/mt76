20d908909951172a8a373ca0024af0358d6223c5
Felix Fietkau <nbd@nbd.name>
2023-08-14 14:10:51 +0200

    wifi: mt76: mt7603: disable A-MSDU tx support on MT7628 It was reported that this can cause the PSE hang issues, even with a low number of fragments.
    Link: openwrt/mt76#793 (comment)
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 2a74b2f0bb7f99f1dd269885e94908b49a41de66)

--- mac80211.c
+++ mac80211.c
@@ -332,7 +332,8 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 	ieee80211_hw_set(hw, SUPPORTS_CLONED_SKBS);
 	ieee80211_hw_set(hw, SUPPORTS_AMSDU_IN_AMPDU);
 	
-	if (!(dev->drv->drv_flags & MT_DRV_AMSDU_OFFLOAD)) {
+	if (!(dev->drv->drv_flags & MT_DRV_AMSDU_OFFLOAD) &&
+	    hw->max_tx_fragments > 1) {
 		ieee80211_hw_set(hw, TX_AMSDU);
 		ieee80211_hw_set(hw, TX_FRAG_LIST);
 	}

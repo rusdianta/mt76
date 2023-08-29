46e53390c59bd717139780c3dc68486068a0b55c
Felix Fietkau <nbd@nbd.name>
2023-08-29 10:39:53 +0200

    wifi: mt76: rename mt76_packet_id_init/flush to mt76_wcid_init/cleanup Also un-inline the functions. Will be used for further wcid init/cleanup
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt76.h
+++ mt76.h
@@ -936,22 +936,7 @@ mt76_mcu_skb_send_msg(struct mt76_dev *dev, struct sk_buff *skb, int cmd,
 
 void mt76_set_irq_mask(struct mt76_dev *dev, u32 addr, u32 clear, u32 set);
 
-static inline void mt76_packet_id_init(struct mt76_wcid *wcid)
-{
-	INIT_LIST_HEAD(&wcid->list);
-	idr_init(&wcid->pktid);
-}
-
-static inline void
-mt76_packet_id_flush(struct mt76_dev *dev, struct mt76_wcid *wcid)
-{
-	struct sk_buff_head list;
-
-	mt76_tx_status_lock(dev, &list);
-	mt76_tx_status_skb_get(dev, wcid, -1, &list);
-	mt76_tx_status_unlock(dev, &list);
-
-	idr_destroy(&wcid->pktid);
-}
+void mt76_wcid_init(struct mt76_wcid *wcid);
+void mt76_wcid_cleanup(struct mt76_dev *dev, struct mt76_wcid *wcid);
 
 #endif

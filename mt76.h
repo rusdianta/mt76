97c28044c484a6508679e86179704a72a908b116
Christian Marangi <ansuelsmth@gmail.com>
2023-10-18 15:09:41 +0200

    wifi: mt76: permit to use alternative cell name to eeprom NVMEM load
    Generilize mt76_get_of_eeprom_from_nvmem to use alternative cell name by
    passing the cell name as an arg and expose it.
    
    Rename it to mt76_get_of_data_from_nvmem to better reflect the now more
    generic usage.
    
    This is to permit driver to load additional cell, like precal cell.
    
    Tested-by: Shiji Yang <yangshiji66@outlook.com>
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit d1c671a90eba873b2990d0d1ed36df1b743c75fe)

--- mt76.h
+++ mt76.h
@@ -759,6 +759,8 @@ void mt76_seq_puts_array(struct seq_file *file, const char *str,
 int mt76_eeprom_init(struct mt76_dev *dev, int len);
 void mt76_eeprom_override(struct mt76_dev *dev);
 int mt76_get_of_data_from_mtd(struct mt76_dev *dev, void *eep, int offset, int len);
+int mt76_get_of_data_from_nvmem(struct mt76_dev *dev, void *eep,
+				const char *cell_name, int len);
 
 static inline u8 *
 mt76_get_txwi_ptr(struct mt76_dev *dev, struct mt76_txwi_cache *t)

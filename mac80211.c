4efb1b25db0d44fee2bcc5bbfdbccee1d4497816
Felix Fietkau <nbd@nbd.name>
2024-04-19 13:33:09 +0200

    wifi: mt76: enable spectrum management It is supported by all drivers
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mac80211.c
+++ mac80211.c
@@ -369,7 +369,8 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 	ieee80211_hw_set(hw, SUPPORTS_CLONED_SKBS);
 	ieee80211_hw_set(hw, SUPPORTS_AMSDU_IN_AMPDU);
 	ieee80211_hw_set(hw, SUPPORTS_REORDERING_BUFFER);
-	
+	ieee80211_hw_set(hw, SPECTRUM_MGMT);
+
 	if (!(dev->drv->drv_flags & MT_DRV_AMSDU_OFFLOAD) &&
 	    hw->max_tx_fragments > 1) {
 		ieee80211_hw_set(hw, TX_AMSDU);

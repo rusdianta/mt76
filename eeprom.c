4bd4dd66ea017535e48023e587cf06e7a70ba6dc
rusdianta <rusdianta@gmail.com>
2024-11-30 01:01:54 +0800

    Fix missing semicolon.

--- eeprom.c
+++ eeprom.c
@@ -306,7 +306,7 @@ s8 mt76_get_rate_power_limits(struct mt76_dev *dev,
 
 	np = mt76_find_channel_node(np, chan);
 	if (!np)
-		return target_power
+		return target_power;
 	txs_delta = mt76_get_txs_delta(np, hweight8(phy->antenna_mask));
 
 	val = mt76_get_of_array(np, "cck", &len, ARRAY_SIZE(dest->cck));

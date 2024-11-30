c6d5732aba06fcdf55d87c6f68c5c5cb80983b34
rusdianta <rusdianta@gmail.com>
2024-12-01 06:03:15 +0800

    no message

--- tx.c
+++ tx.c
@@ -411,8 +411,8 @@ mt76_txq_send_burst(struct mt76_dev *dev, struct mt76_sw_queue *sq,
 		if (probe)
 			break;
 
-		if (test_bit(MT76_STATE_PM, &phy->state) ||
-		    test_bit(MT76_RESET, &phy->state))
+		if (test_bit(MT76_STATE_PM, &dev->state) ||
+		    test_bit(MT76_RESET, &dev->state))
 			return -EBUSY;
 
 		skb = mt76_txq_dequeue(dev, mtxq, false);
@@ -464,8 +464,8 @@ mt76_txq_schedule_list(struct mt76_dev *dev, enum mt76_txq_id qid)
 		if (sq->swq_queued >= 4)
 			break;
 
-		if (test_bit(MT76_STATE_PM, &phy->state) ||
-		    test_bit(MT76_RESET, &phy->state)) {
+		if (test_bit(MT76_STATE_PM, &dev->state) ||
+		    test_bit(MT76_RESET, &dev->state)) {
 			ret = -EBUSY;
 			break;
 		}

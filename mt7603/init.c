041afef359e4715db3c74cf360da352a6acf9f72
rusdianta <rusdianta@gmail.com>
2024-12-25 10:25:12 +0800

    Reverted mt76: mt7603: check for single-stream EEPROM configuration Some devices using MT7628 or MT7603 have only one antenna chain connected. Detect these using the EEPROM rx/tx path settings

--- mt7603/init.c
+++ mt7603/init.c
@@ -511,17 +511,11 @@ mt7603_init_txpower(struct mt7603_dev *dev,
 	}
 }
 
-static inline bool is_mt7688(struct mt7603_dev *dev)
-{
-	return mt76_rr(dev, MT_EFUSE_BASE + 0x64) & BIT(4);
-}
-
 int mt7603_register_device(struct mt7603_dev *dev)
 {
 	struct mt76_bus_ops *bus_ops;
 	struct ieee80211_hw *hw = mt76_hw(dev);
 	struct wiphy *wiphy = hw->wiphy;
-	u8 *eeprom = (u8 *)dev->mt76.eeprom.data;
 	int ret;
 
 	dev->bus_ops = dev->mt76.bus;
@@ -543,11 +537,9 @@ int mt7603_register_device(struct mt7603_dev *dev)
 	tasklet_init(&dev->mt76.pre_tbtt_tasklet, mt7603_pre_tbtt_tasklet,
 		     (unsigned long)dev);
 
-	/* Check for 1SS devices */
+	/* Check for 7688, which only has 1SS */
 	dev->mt76.antenna_mask = 3;
-	if (FIELD_GET(MT_EE_NIC_CONF_0_RX_PATH, eeprom[MT_EE_NIC_CONF_0]) == 1 ||
-	    FIELD_GET(MT_EE_NIC_CONF_0_TX_PATH, eeprom[MT_EE_NIC_CONF_0]) == 1 ||
-	    is_mt7688(dev))
+	if (mt76_rr(dev, MT_EFUSE_BASE + 0x64) & BIT(4))
 		dev->mt76.antenna_mask = 1;
 
 	dev->slottime = 9;

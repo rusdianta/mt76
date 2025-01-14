43579911d998c8286288d3139035b302237411c6
rusdianta <rusdianta@gmail.com>
2025-01-14 10:34:27 +0800

    Optimize variable declaration for dma.c

--- dma.c
+++ dma.c
@@ -369,12 +369,13 @@ static void *
 mt76_dma_dequeue(struct mt76_dev *dev, struct mt76_queue *q, bool flush,
 		 int *len, u32 *info, bool *more)
 {
-	int idx = q->tail;
+	int idx;
 
 	*more = false;
 	if (!q->queued)
 		return NULL;
 
+	idx = q->tail;
 	if (flush)
 		q->desc[idx].ctrl |= cpu_to_le32(MT_DMA_CTL_DMA_DONE);
 	else if (!(q->desc[idx].ctrl & cpu_to_le32(MT_DMA_CTL_DMA_DONE)))

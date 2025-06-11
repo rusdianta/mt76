ca0807972c520347eaacaba439ce3fef51c4e58f
rusdianta <rusdianta@gmail.com>
2025-06-11 18:51:20 +0800

    mt76: mt7603: never use an 802.11b CF-End rate on 5GHz Sometimes mt7615_mac_set_timing gets called while the slot time is still configured to 20. Ensure that in this case it always uses the OFDM CFend rate.
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 5349dc40abb3e2c819df020594046baf01940db3)
    (cherry picked from commit 7a00a1d5a9da99bd3c90c390b266502be14aab51)
    (cherry picked from commit ad471b86de476c6e83f0aab52686c1a4a617d905)

--- mt7603/mac.c
+++ mt7603/mac.c
@@ -50,10 +50,11 @@ void mt7603_mac_set_timing(struct mt7603_dev *dev)
 	int offset = 3 * dev->coverage_class;
 	u32 reg_offset = FIELD_PREP(MT_TIMEOUT_VAL_PLCP, offset) |
 			 FIELD_PREP(MT_TIMEOUT_VAL_CCA, offset);
+	bool is_5ghz = dev->mt76.chandef.chan->band == NL80211_BAND_5GHZ;
 	int sifs;
 	u32 val;
 
-	if (dev->mt76.chandef.chan->band == NL80211_BAND_5GHZ)
+	if (is_5ghz)
 		sifs = 16;
 	else
 		sifs = 10;
@@ -70,7 +71,7 @@ void mt7603_mac_set_timing(struct mt7603_dev *dev)
 		FIELD_PREP(MT_IFS_SIFS, sifs) |
 		FIELD_PREP(MT_IFS_SLOT, dev->slottime));
 
-	if (dev->slottime < 20)
+	if (dev->slottime < 20 || is_5ghz)
 		val = MT7603_CFEND_RATE_DEFAULT;
 	else
 		val = MT7603_CFEND_RATE_11B;

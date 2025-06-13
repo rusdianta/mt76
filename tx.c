5c59449270501906f2d1bdc195218f98dc743607
rusdianta <rusdianta@gmail.com>
2025-06-13 23:09:28 +0800

    mt76: check return value of mt76_txq_send_burst in mt76_txq_schedule_list Since mt76_txq_send_burst routine can report a negative error code, check the returned value before incrementing the number of transmitted frames in mt76_txq_schedule_list routine. Return -EBUSY directly if the device is in reset or in power management.
    Fixes: 90fdc1717b186 ("mt76: use mac80211 txq scheduling")
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    (cherry picked from commit 43e9f05c4ba465416acf0ae8848935148c4a3151)
    (cherry picked from commit a849c6f60f8398bb3bec8cef5ce1203bb696f0da)

--- tx.c
+++ tx.c
@@ -398,13 +398,13 @@ mt76_txq_schedule_list(struct mt76_dev *dev, enum mt76_txq_id qid)
 	int ret = 0;
 
 	while (1) {
+		int n_frames = 0;
+
 		if (sq->swq_queued >= 4)
 			break;
 
-		if (test_bit(MT76_RESET, &dev->state)) {
-			ret = -EBUSY;
-			break;
-		}
+		if (test_bit(MT76_RESET, &dev->state))
+			return -EBUSY;
 
 		if (dev->queue_ops->tx_cleanup &&
 		    hwq->queued + 2 * MT_TXQ_FREE_THR >= hwq->ndesc) {
@@ -435,12 +435,17 @@ mt76_txq_schedule_list(struct mt76_dev *dev, enum mt76_txq_id qid)
 			spin_lock_bh(&hwq->lock);
 		}
 
-		ret += mt76_txq_send_burst(dev, sq, mtxq);
+		n_frames = mt76_txq_send_burst(dev, sq, mtxq);
 
 		spin_unlock_bh(&hwq->lock);
 		
 		ieee80211_return_txq(dev->hw, txq,
 				     !skb_queue_empty(&mtxq->retry_q));
+
+		if (unlikely(n_frames < 0))
+			return n_frames;
+
+		ret += n_frames;
 	}
 
 	return ret;

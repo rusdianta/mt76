9511fdcf2e73239ea637ba2fde7d6204905195fa
David Bauer <mail@david-bauer.net>
2026-06-11 23:56:56 +0200

    wifi: mt76: mt7603: free beacon SKB on error
    The SKB containing the generated beacon is not freed when the beacon
    queue is deected stuck and scheduled for recovery.
    
    Fixes potential memory leaks in case the beacon queue is detected stuck.
    
    Signed-off-by: David Bauer <mail@david-bauer.net>
    Link: https://patch.msgid.link/20260611215658.259324-1-mail@david-bauer.net
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mt7603/beacon.c
+++ mt7603/beacon.c
@@ -57,6 +57,7 @@ mt7603_update_beacon_iter(void *priv, u8 *mac, struct ieee80211_vif *vif)
 		FIELD_PREP(MT_DMA_FQCR0_TARGET_QID, MT_TX_HW_QUEUE_BCN));
 	if (!mt76_poll(dev, MT_DMA_FQCR0, MT_DMA_FQCR0_BUSY, 0, 5000)) {
 		dev->beacon_check = MT7603_WATCHDOG_TIMEOUT;
+		dev_kfree_skb(skb);
 		goto out;
 	}
 
@@ -64,6 +65,7 @@ mt7603_update_beacon_iter(void *priv, u8 *mac, struct ieee80211_vif *vif)
 		FIELD_PREP(MT_DMA_FQCR0_TARGET_QID, MT_TX_HW_QUEUE_BMC));
 	if (!mt76_poll(dev, MT_DMA_FQCR0, MT_DMA_FQCR0_BUSY, 0, 5000)) {
 		dev->beacon_check = MT7603_WATCHDOG_TIMEOUT;
+		dev_kfree_skb(skb);
 		goto out;
 	}
 

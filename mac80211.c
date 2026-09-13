436c125c23dd73d66b190c518515995222cf2886
rusdianta <rusdianta@gmail.com>
2026-09-13 22:49:15 +0800

    wifi: mt76: do not run mt76_unregister_device() on unregistered hw Trying to probe a mt7921e pci card without firmware results in a successful probe where ieee80211_register_hw hasn't been called. When removing the driver, ieee802111_unregister_hw is called unconditionally leading to a kernel NULL pointer dereference. Fix the issue running mt76_unregister_device routine just for registered hw.
    Link: https://bugs.debian.org/1029116
    Link: https://bugs.kali.org/view.php?id=8140
    Reported-by: Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
    Fixes: 1c71e03afe4b ("mt76: mt7921: move mt7921_init_hw in a dedicated work")
    Tested-by: Helmut Grohne <helmut@freexian.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mac80211.c
+++ mac80211.c
@@ -407,6 +407,8 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 	if (ret)
 		return ret;
 
+	set_bit(MT76_STATE_REGISTERED, &dev->state);
+
 	WARN_ON(mt76_worker_setup(hw, &dev->tx_worker, NULL, "tx"));
 	sched_setscheduler(dev->tx_worker.task, SCHED_FIFO, &sparam);
 
@@ -417,6 +419,9 @@ EXPORT_SYMBOL_GPL(mt76_register_device);
 void mt76_unregister_device(struct mt76_dev *dev)
 {
 	struct ieee80211_hw *hw = dev->hw;
+
+	if (!test_bit(MT76_STATE_REGISTERED, &dev->state))
+		return;
 
 	if (IS_ENABLED(CONFIG_MT76_LEDS))
 		mt76_led_cleanup(dev);

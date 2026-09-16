5571ebeb7583b674284ca6aaf3a8997b96304ea4
rusdianta <rusdianta@gmail.com>
2026-09-17 00:16:25 +0800

    wifi: mt76: mt7996: add missing max_remain_on_channel_duration Having this unset breaks remain-on-channel and mgmt TX. Move setting it to mt76 core to keep it in one place.
    Fixes: 69d54ce7491d0 ("wifi: mt76: mt7996: switch to single multi-radio wiphy")
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- mac80211.c
+++ mac80211.c
@@ -358,6 +358,9 @@ int mt76_register_device(struct mt76_dev *dev, bool vht,
 			WIPHY_FLAG_SUPPORTS_TDLS |
 			WIPHY_FLAG_AP_UAPSD;
 
+	if (!wiphy->max_remain_on_channel_duration)
+		wiphy->max_remain_on_channel_duration = 5000;
+
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_CQM_RSSI_LIST);
 	wiphy_ext_feature_set(wiphy, NL80211_EXT_FEATURE_AIRTIME_FAIRNESS);
 
